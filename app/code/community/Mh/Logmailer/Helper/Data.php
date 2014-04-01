2b81da3a411de082c81c67c378f3a0d91840e93d
mat <matthias_haupt@gmx.de>
2014-04-01 20:26:52 +0200

    init

---
+++ app/code/community/Mh/Logmailer/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Matthias Haupt
+ * www.matthias-haupt.net
+ *
+ * @author     Matthias Haupt matthias@matthias-haupt.net
+ * @date       01.04.14
+ * @license    MIT http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * Class Mh_Logmailer_Helper_Data
+ */
+class Mh_Logmailer_Helper_Data extends Mage_Core_Helper_Data
+{
+
+}
